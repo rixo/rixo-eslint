a0d13a2fe065474e4dbb4365af4699b4a10cd8d2
rixo <eric@rixo.fr>
2019-05-17 14:29:48 +0200

    add init script

---
+++ README.md
@@ -0,0 +1,15 @@
+Monorepo for all my eslint & prettier stuff, in a sharable form.
+
+# Installation
+
+```bash
+npm install --dev @rixo/eslint-config
+```
+
+# Usage
+
+The following script will create defaults `.eslintrc` and `.prettierrc` where it is run (should be the root of your project, in most cases).
+
+```bash
+npx rixo-eslint init
+```
---
+++ packages/eslint/bin/init.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const encoding = 'utf8'
+
+const cwd = process.cwd()
+const files = ['.eslintrc', '.prettierrc']
+
+if (process.argv[2] !== 'init') {
+  console.log('Usage: rixo-eslint init')
+}
+
+const write = (file, contents) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(`${cwd}/${file}`, contents, encoding, err => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+
+const readTpl = file =>
+  new Promise((resolve, reject) => {
+    fs.readFile(
+      `${__dirname}/../templates/${file}`,
+      { encoding },
+      (err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      }
+    )
+  })
+
+const processFile = async file => {
+  const contents = await readTpl(file)
+  await write(file, contents)
+  console.info(`Written: ${file}`)
+}
+
+Promise.all(files.map(processFile))
+  .then(() => console.log('OK'))
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
