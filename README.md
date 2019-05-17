6922b0eb1be5d176ad963edfd16c3c41f06873e8
rixo <eric@rixo.fr>
2019-05-17 15:34:15 +0200

    create .eslintrc & .prettierrc in postinstall

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Monorepo for all my eslint & prettier stuff, in a easily sharable form.
 npm install --dev @rixo/eslint-config
 ```
 
+A `.eslintrc` and `.prettierrc` will be created in your project by a npm postinstall script. If these files already exists in your project, then they won't be touched.
+
+In some cases, depending on the order you run your `npm install` commands, the files may not be created or may target the wrong variant. In case of problem, refer to the [variants](#variants) section bellow and fix it!
+
 # Usage
 
 The following script will create defaults `.eslintrc` and `.prettierrc` where it is run (should be the root of your project, in most cases).
@@ -16,10 +20,16 @@ npx rixo-eslint init
 
 # Variants
 
-## Atom
-
-`.eslintrc`
+## Default
 
 ```yml
+# .eslintrc
+extends: '@rixo'
+```
+
+## Atom
+
+```yml
+# .eslintrc
 extends: '@rixo/atom'
 ```
---
+++ packages/eslint/postinstall.js
@@ -0,0 +1,110 @@
+const fs = require('fs')
+const path = require('path')
+
+const encoding = 'utf8'
+
+const files = ['.eslintrc', '.prettierrc']
+const sortedModulesWithTemplates = [
+  '@rixo/eslint-config-atom',
+  '@rixo/eslint-config',
+  '@rixo/eslint',
+]
+
+const resolveTargets = async () => {
+  const resolved = {}
+  for (const moduleName of sortedModulesWithTemplates) {
+    try {
+      const index = require.resolve(moduleName)
+      if (!index) return
+      const dir = path.dirname(index)
+      const templates = `${dir}/templates`
+      for (const file of files) {
+        if (!resolved[file]) {
+          const path = `${templates}/${file}`
+          const exists = fs.existsSync(path)
+          if (exists) {
+            console.info(`Found ${file} in ${moduleName}: ${path}`)
+            resolved[file] = await readTpl(path)
+            const allResolved = Object.keys(resolved).length === files.length
+            if (allResolved) {
+              return resolved
+            }
+          }
+        }
+      }
+    } catch (err) {
+      // rethrow unexpected errors
+      if (!/Error: Cannot find module/.test(err)) {
+        throw err
+      }
+      console.debug(`Optional module not found: ${moduleName}`)
+    }
+  }
+  return resolved
+}
+
+const writeFile = (path, contents) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(path, contents, encoding, err => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+
+const resolveAppRoot = () => {
+  const steps = __dirname.split('/')
+  steps.pop()
+  while (steps.length) {
+    const path = [...steps].join('/') || '/'
+    steps.pop()
+    const found =
+      fs.existsSync(`${path}/package.json`) ||
+      fs.existsSync(`${path}/node_modules`)
+    if (found) {
+      return path
+    }
+  }
+}
+
+const writeTarget = async ([file, contents]) => {
+  const cwd = resolveAppRoot()
+  if (!cwd) {
+    throw new Error('Failed to find project root from: ' + __dirname)
+  }
+  const target = `${cwd}/${file}`
+  if (fs.existsSync(file)) {
+    console.info(`Skipping existing file: ${file}`)
+    return
+  }
+  await writeFile(target, contents)
+  console.info(`Written: ${file}`)
+}
+
+const readTpl = path =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding }, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+
+const warnMissingFiles = targets => {
+  files.forEach(file => {
+    if (!targets[file]) {
+      console.warn(`No suitable ${file} found in your installed module`)
+      console.info(`${file} won't be automatically created`)
+    }
+  })
+  return targets
+}
+
+Promise.resolve()
+  .then(resolveTargets)
+  .then(warnMissingFiles)
+  .then(targets => Object.entries(targets))
+  .then(targets => Promise.all(targets.map(writeTarget)))
+  .then(() => console.log('OK'))
+  .catch(err => {
+    console.error(err)
+    process.exit(255)
+  })
