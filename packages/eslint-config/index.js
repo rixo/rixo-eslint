82a64ab2f01654cb087e2bb7886f0599cf3e7470
rixo <eric@rixo.fr>
2019-05-17 17:49:45 +0200

    fix eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,5 @@
 const { readConfig } = require('@rixo/eslint')
 
-const CONFIG_FILE = __dirname + '/configs/.eslintrc.yml'
+const CONFIG_FILE = __dirname + '/configs/.eslintrc'
 
 module.exports = readConfig(CONFIG_FILE)
